c5709d253f879284d687e44265dc8c64e624206b
Brandon Benvie <brandon@bbenvie.com>
2012-03-17 20:11:31 -0400

    Initial tests for numeric

---
+++ test/numeric.js
@@ -0,0 +1,80 @@
+var tap = require("tap");
+var test = tap.test;
+var Buffer = require('../lib/buffer');
+var numeric;
+var Int8;
+
+test('load', function(t){
+  numeric = require('../lib/numeric');
+  t.ok(numeric, 'numerics loaded');
+  t.equal(Object.keys(numeric).sort()+'', "Double,Float,Int16,Int32,Int8,UInt16,UInt32,UInt8,isInstance", "numerics has all expected names");
+  t.end();
+});
+
+test('constructor properties', function(t){
+  Int8 = numeric.Int8;
+  t.equal(typeof Int8, 'function', 'ctor is function');
+  t.equal(typeof Int8.isInstance, 'function', 'isInstance is function');
+  t.equal(Int8.name, 'Int8', 'named');
+  t.equal(Int8.bytes, 1, 'correct bytes');
+  t.equal(Int8.__proto__, numeric.prototype, 'ctor inherits from numeric.prototype');
+  t.equal(Int8+'', '[object Type]', 'toString shows class "Type"');
+  t.end();
+});
+
+test('prototype properties', function(t){
+  t.equal(Int8.bytes, Int8.prototype.bytes, 'prototype bytes match constructor bytes');
+  t.equal(Int8.prototype.__proto__, numeric.prototype.prototype, 'prototype inherits from numeric.prototype.prototype');
+  t.equal(Int8.prototype.DataType, 'Int8', 'DataType is "Int8"');
+  t.equal(Int8.prototype+'', '[object Data]', 'toString shows class "Data"');
+  t.end();
+});
+
+test('buffer auto-allocate', function(t){
+  var int8 = new Int8(100);
+  t.ok(int8, 'constructed');
+  t.equal(int8.__proto__, Int8.prototype, 'instance inherits from ctor.prototype');
+  t.equal(int8.reify(), 100, 'reified value matches value given to ctor');
+  t.equal(int8.buffer.length, 1, 'buffer is correct size');
+  t.equal(int8.buffer[0], 100, 'buffer value matches given value');
+  t.end();
+});
+
+test('provided buffer', function(t){
+  var buffer = new Buffer(1);
+  buffer[0] = 30;
+  var int8 = new Int8(buffer);
+  t.equal(int8.buffer, buffer, 'buffer property matches given buffer');
+  t.equal(int8.reify(), 30, 'reifies to value preset on buffer');
+  int8.write(50);
+  t.equal(buffer[0], 50, 'buffer matches newly written value');
+  int8 = new Int8(buffer, 0, 100);
+  t.equal(buffer[0], 100, 'setting value during construction works');
+  t.end();
+});
+
+test('shared buffer', function(t){
+  var first = new Int8(100);
+  var second = new Int8(first);
+  t.notEqual(first, second, 'different wrappers');
+  t.equal(first.buffer, second.buffer, 'have same buffer');
+  t.equal(first.offset, second.offset, 'offsets match');
+  second.write(50);
+  t.equal(first.reify(), 50, 'writing to one alters the other');
+  t.end();
+});
+
+test('value sources', function(t){
+  var int8 = new Int8();
+  t.ok(int8.write(new Int8(100)), 'writing using a Data instance works');
+  t.equal(int8.reify(), 100, 'value written correctly');
+  t.end();
+});
+
+test('limits', function(t){
+  t.throws(function(){ new Int8(300) }, 'throw on construct with out of bounds value');
+  var int8 = new Int8;
+  t.throws(function(){ int8.write(400) }, 'throw on write out of bounds value');
+  t.throws(function(){ int8.write(new Int8(500)) }, 'throw on write out of bounds Data instance');
+  t.end();
+});
