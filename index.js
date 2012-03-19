ecbc6787b923804dfb6b7957effc20e69fef741b
Brandon Benvie <brandon@bbenvie.com>
2012-03-18 21:05:58 -0400

    Accessor for endianness

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+var ViewBuffer = require('./lib/buffer');
+
 module.exports = {
   Numeric:  require('./lib/numeric'),
   Struct:   require('./lib/struct'),
   Array:    require('./lib/array'),
   Bitfield: require('./lib/bitfield'),
+  ViewBuffer: ViewBuffer,
+  get defaultEndian(){
+  	return ViewBuffer.prototype.endianness;
+  },
+  set defaultEndian(v){
+  	if (v !== 'LE' && v !== 'BE') throw new Error('Endianness must be "BE" or "LE"');
+  	ViewBuffer.prototype.endianness = v;
+  }
 };
