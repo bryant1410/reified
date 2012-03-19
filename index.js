c0422aaf22ae8c89d04c90a5a571d57c2ace9b5f
Brandon Benvie <brandon@bbenvie.com>
2012-03-19 04:58:16 -0400

    Update export to be a function that creates Data instances by Type name

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 var ViewBuffer = require('./lib/buffer');
+var lookupType = require('./lib/genesis').lookupType;
 
-module.exports = {
-  Numeric:  require('./lib/numeric'),
-  Struct:   require('./lib/struct'),
-  Array:    require('./lib/array'),
-  Bitfield: require('./lib/bitfield'),
-  ViewBuffer: ViewBuffer,
-  get defaultEndian(){
+module.exports = createData;
+
+function createData(type, buffer, offset, values){
+  type = lookupType(type);
+  return new type(buffer, offset, values);
+}
+
+createData.Numeric = require('./lib/numeric');
+createData.Struct = require('./lib/struct');
+createData.Array = require('./lib/array');
+createData.Bitfield = require('./lib/bitfield');
+createData.ViewBuffer = ViewBuffer;
+
+Object.defineProperty(createData, 'defaultEndian', {
+  enumerable: true,
+  configurable: true,
+  get: function(){
   	return ViewBuffer.prototype.endianness;
   },
-  set defaultEndian(v){
+  set: function(v){
   	if (v !== 'LE' && v !== 'BE') throw new Error('Endianness must be "BE" or "LE"');
   	ViewBuffer.prototype.endianness = v;
   }
-};
+});
